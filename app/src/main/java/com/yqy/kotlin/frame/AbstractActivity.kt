3a1cc63720edc93b581f2bb0018fcda59bf217d2
Derek_Yan <yanjiuyangok@163.com>
2017-07-12 23:55:29 +0800

    自从开始学习kotlin，头不痛了，腰不酸了

--- app/src/main/java/com/yqy/kotlin/frame/AbstractActivity.kt
+++ app/src/main/java/com/yqy/kotlin/frame/AbstractActivity.kt
@@ -2,13 +2,27 @@ package com.yqy.kotlin.frame
 
 import android.app.ProgressDialog
 import android.content.Context
+import android.content.DialogInterface
+import android.os.IBinder
+import android.support.design.widget.Snackbar
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.yqy.kotlin.R
 import com.yqy.kotlin.http.SubscriberResultListener
+import com.yqy.kotlin.listener.OnAlertDialogListener
 import com.yqy.kotlin.listener.OnClickBackListener
+import com.yqy.kotlin.utils.glide.GlideCircleTransform
 
 /**
  * acivity基类
@@ -16,7 +30,7 @@ import com.yqy.kotlin.listener.OnClickBackListener
  */
 abstract class AbstractActivity : AppCompatActivity(), View.OnClickListener,SubscriberResultListener<Object>{
     var mProgressDialog: ProgressDialog? = null
-    var mAlertDialog: AlertDialog? = null
+    var mAlertDialog: AlertDialog.Builder? = null
     var mContext: Context? = null
 
     var pageNum: Int = 20 //分页 每页数量
@@ -67,10 +81,148 @@ abstract class AbstractActivity : AppCompatActivity(), View.OnClickListener,Subs
     }
 
     /**
+     * 隐藏软键盘
+     */
+    fun hideSoftInput(edittext: EditText){
+        try {
+            hideSoftInput(edittext.windowToken)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * 隐藏软键盘
+     */
+    fun hideSoftInput(binder: IBinder?){
+        try {
+            val inputMethodManager: InputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(binder,0)
+        } catch(e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * 加载圆形图片
+     */
+    fun loadCircleImg(url: String, mImageView: ImageView){
+        Glide.with(this).load(url)
+                .placeholder(R.mipmap.ic_launcher)          //加载前图片
+                .error(R.mipmap.ic_launcher)                //加载失败图片
+                .transform(GlideCircleTransform(this))      //切圆形
+                .diskCacheStrategy(DiskCacheStrategy.ALL)   //缓存 全尺寸和适应缓存
+                .into(mImageView)
+    }
+
+    /**
+     * 加载普通图片
+     */
+    fun loadImg(url: String, mImageView: ImageView){
+        Glide.with(this).load(url)
+                .placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.ic_launcher)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(mImageView)
+    }
+
+    /**
+     * Snackbar 根布局下方提示的类似Toast 用户可滑动删除
+     */
+    fun showSnackbar(tip: String){
+        Snackbar.make(this.window.decorView.findViewById(android.R.id.content),
+                tip,
+                Snackbar.LENGTH_SHORT)
+                .show()
+    }
+
+    /**
      * Toast
      */
     fun showToast(tip: String) {
         Toast.makeText(this, tip, Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * 展示对话框
+     */
+    fun showAlertDialog(messageStr: String?, //消息
+                        cancelStr: String,  //关闭提示文字
+                        rightStr: String,   //右侧确认提示文字
+                        mListener: OnAlertDialogListener){
+        if(mAlertDialog == null) mAlertDialog = AlertDialog.Builder(this)
+        mAlertDialog!!.setMessage(messageStr)
+                .setPositiveButton(rightStr, DialogInterface.OnClickListener { dialog, which ->
+                    dialog.dismiss()
+                    mListener.onNegative() })
+                .setNegativeButton(cancelStr,DialogInterface.OnClickListener { dialog, which ->
+                    dialog.dismiss()
+                    mListener.onNegative()
+                }).show()
+    }
+
+    /**
+     * 加载进度框
+     */
+    fun showProgressDialog(messageStr: String?){
+        if(mProgressDialog == null) {
+            mProgressDialog = ProgressDialog(this)
+            mProgressDialog!!.isIndeterminate = true
+            mProgressDialog!!.setCancelable(true)
+        }
+        if(TextUtils.isEmpty(messageStr)) mProgressDialog!!.setMessage("加载中...")
+        else mProgressDialog!!.setMessage(messageStr)
+        if(mProgressDialog!!.isShowing)
+            mProgressDialog!!.dismiss()
+        mProgressDialog!!.show()
+    }
+
+    fun dissmissProgressDialog(){
+        if(mProgressDialog!!.isShowing)
+            mProgressDialog!!.dismiss()
+    }
+
+    /**
+     * 拦截返回键
+     */
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            //拦截系统返回键单击事件
+            mOnClickBackListener = getOnBackClickListener()
+            if(mOnClickBackListener != null) {
+                //触发我们自己的返回键监听事件
+                mOnClickBackListener!!.onClickBack()
+                return true
+            }
+            finish()
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    /**
+     * 倒计时相关
+     */
+    var sendMsmCount = 60
+    fun startTimeDown(tv: TextView, sendMsmCount: Int){
+        tv.isEnabled = false
+        tv.tag = sendMsmCount
+        tv.postDelayed(object : Runnable {
+            override fun run() {
+                var tag = tv.tag as Int
+                tag--
+                if (tag >= 0) {
+                    tv.text = tag.toString() + "s"
+                    tv.tag = tag
+                    tv.postDelayed(this, 1000)
+                } else if (tag >= -1) {
+                    tv.text = "获取验证码"
+                    tv.isEnabled = true
+                } else {
+                    tv.text = "获取验证码"
+                }
+            }
+        }, 1000)
     }
 
 }
